a1dd85dd0eb27b7b56c3e067f5d8b7bda5e5de0e
Cerus <github.social@cerus.dev>
2023-03-02 19:40:30 +0100

    Add option to specify forks

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #/bin/sh
 
 # -- Clone the repo
-git clone https://github.com/Xjph/ObservatoryCore
+if [[ $1 == "" ]]
+then
+  # Build official repo
+  echo "Building Xjph/ObservatoryCore"
+  git clone https://github.com/Xjph/ObservatoryCore
+else
+  # Build fork
+  echo "Building $1/ObservatoryCore"
+  git clone https://github.com/$1/ObservatoryCore
+fi
 cd ObservatoryCore
+if [[ $2 != "" ]]
+then
+  # Build custom branch
+  git fetch
+  git checkout $2
+fi
 
 # -- Build the repo
 # Build everything
